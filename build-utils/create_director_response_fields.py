d02c0e078cd7d5a26e48ed98a7aaeac3afccfa98
Christian Ledermann <christian.ledermann@gmail.com>
2014-11-14 16:17:19 +0000

    housekeeping, move constants into apiconsts rename test_main to test_pro, pep8, isort

---
+++ build-utils/create_director_response_fields.py
@@ -0,0 +1,27 @@
+import csv
+
+with open('director-key-info-response.csv', 'rb') as csv_in_file:
+    reader = csv.DictReader(csv_in_file)
+    f_list = []
+    print '# key info'
+    for row in reader:
+        var = row['Field Name']
+        print "'%s'," % var
+        print '#', row['Type'], row['Description']
+
+print
+print '#########################'
+print
+
+with open('director-directorships.csv', 'rb') as csv_in_file:
+    reader = csv.DictReader(csv_in_file)
+    f_list = []
+    print '# directorships'
+    for row in reader:
+        var = row['Field Name']
+        print "'%s'," % var
+        print '#', row['Type'], row['Description']
+
+print
+print '#########################'
+print
