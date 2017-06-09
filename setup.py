efd62fd28bf19fae7ffdecd46b1d1e18c6b6027f
Bruno Alla <bruno.alla@founders4schools.org.uk>
2017-06-09 10:48:25 +0100

    Add throve classifiers

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(name='duedil',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
                    'Development Status :: 3 - Alpha', ],
       # Get strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
